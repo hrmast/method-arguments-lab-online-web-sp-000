5d0a97cb6e4215633c36fbc04e4187d84e0636d4
Heath Mast <hrmast@gmail.com>
2020-04-02 15:30:44 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,7 @@
 def introduction(name)
   puts "Hi, my name is #{name}.\n"
 end
+
+def introduction_with_language(language, name)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
